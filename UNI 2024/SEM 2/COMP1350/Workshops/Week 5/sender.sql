a35a6a1e3b5b77ea7b7f93706b12c97136e531de
slugman <tyler.adrian.johnson@gmail.com>
2024-08-20 20:19:28 +1000

    vault backup: 2024-08-20 20:19:28

--- UNI 2024/SEM 2/COMP1350/Workshops/Week 5/sender.sql
+++ UNI 2024/SEM 2/COMP1350/Workshops/Week 5/sender.sql
@@ -1,8 +1,10 @@
 create table Sender(
-	senderID int,
+	senderID varchar(4),
     SenderName varchar(255),
-    SenderDOB varchar(255),
+    SenderDOB date,
     SenderEmail varchar(255),
     SenderAddress varchar(255),
     primary key (senderID)
 );
+
+insert into Sender values ('S001', 'Rick Sanchez', 1985-03-17, 'rickthescientist@dimension.gov', '42 Wallaby Lane, Sydney NSW 2000');

e6047e7b434c7510989d1ad56983e40339f6c0c0
slugman <tyler.adrian.johnson@gmail.com>
2024-08-20 20:15:11 +1000

    vault backup: 2024-08-20 20:15:11

---
+++ UNI 2024/SEM 2/COMP1350/Workshops/Week 5/sender.sql
@@ -0,0 +1,8 @@
+create table Sender(
+	senderID int,
+    SenderName varchar(255),
+    SenderDOB varchar(255),
+    SenderEmail varchar(255),
+    SenderAddress varchar(255),
+    primary key (senderID)
+);

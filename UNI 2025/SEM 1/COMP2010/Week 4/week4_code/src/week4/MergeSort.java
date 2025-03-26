e6619a4fb2bbef5340b914874b9fc638c5f60692
slugman <tyler.adrian.johnson@gmail.com>
2025-03-26 17:18:58 +1100

    hhh

--- UNI 2025/SEM 1/COMP2010/Week 4/week4_code/src/week4/MergeSort.java
+++ UNI 2025/SEM 1/COMP2010/Week 4/week4_code/src/week4/MergeSort.java
@@ -7,21 +7,21 @@ import static org.junit.Assert.*;
 
 public class MergeSort {
     
-    public static char[] sort(char[] unsorted){
+    public static char[] sort(char[] unsorted, boolean upDown){
         if (unsorted.length == 1)
           return unsorted;
         int    mid   = unsorted.length/2;
-        char[] left  = sort(Arrays.copyOfRange(unsorted, 0, mid));
-        char[] right = sort(Arrays.copyOfRange(unsorted, mid, unsorted.length));
-        return merge(left, right);
+        char[] left  = sort(Arrays.copyOfRange(unsorted, 0, mid), upDown);
+        char[] right = sort(Arrays.copyOfRange(unsorted, mid, unsorted.length), upDown);
+        return merge(left, right, upDown);
     }
 
-    public static char[] merge(char[] sorted_a, char[] sorted_b){
+    public static char[] merge(char[] sorted_a, char[] sorted_b, boolean upDown){
         char[] ret = new char[sorted_a.length + sorted_b.length];
         int i = 0;
         int j = 0;
         while (i < sorted_a.length && j < sorted_b.length){
-            if(sorted_a[i] < sorted_b[j]){
+            if((upDown && sorted_a[i] < sorted_b[j]) || (!upDown && sorted_a[i] > sorted_b[j])){
                 ret[i+j] = sorted_a[i];
                 i++;
             } else {
@@ -29,6 +29,7 @@ public class MergeSort {
                 j++;                
             }
         }
+
         // flush the left
         while (i < sorted_a.length){
             ret[i+j] = sorted_a[i];
@@ -43,10 +44,22 @@ public class MergeSort {
         return ret;
     }
 
+    public static void main(String[] args) {
+        char[] arr = {'g','a','c','d','e','f','j','k','j'};
+        System.out.println(sort(arr, true ));
+    }
+
+
     @Test
     public void testBasics(){
-        assertArrayEquals(sort(new char[]{'b','a'}), new char[]{'a','b'});
-        assertArrayEquals(sort(new char[]{'b','a','j'}), new char[]{'a','b','j'});
-        assertArrayEquals(sort(new char[]{'b','a','c','d','e','f','j','k','j'}), new char[]{'a','b','c','d','e','f','j','j','k'});
+        //Ascending
+        assertArrayEquals(sort(new char[]{'b','a'}, true), new char[]{'a','b'});
+        assertArrayEquals(sort(new char[]{'b','a','j'}, true), new char[]{'a','b','j'});
+        assertArrayEquals(sort(new char[]{'b','a','c','d','e','f','j','k','j'}, true), new char[]{'a','b','c','d','e','f','j','j','k'});
+        
+        //Descending
+        assertArrayEquals(sort(new char[]{'g','a','c','d','e','f','j','k','j'}, false), new char[]{'k','j','j','g','f','e','d','c','a'});
+        assertArrayEquals(sort(new char[]{'a','b'}, false), new char[]{'b','a'});
+        assertArrayEquals(sort(new char[]{'b','a','j'}, false), new char[]{'j','b','a'});
     }
 }
